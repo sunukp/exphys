5b99c6751b32533d684c305dcfc679198ece1e49
Sunuk Park <sunukp0@gmail.com>
2023-09-11 01:46:12 -0700

    using np.allcose() for test results

--- src/tests/test_matfcns.py
+++ src/tests/test_matfcns.py
@@ -5,30 +5,35 @@ PRECISION=4
 
 
 if __name__ == '__main__':
-    assert np.round(temperature2density(12), PRECISION) == 1.238
+    assert np.isclose(temperature2density(12), 1.23796)
 
     v_as=np.array([5, 8, 9])
     rhos=[1.4, 2, 3]
-    assert np.round(dragF(v_as[0], rhos[0], area=0.5089), PRECISION) == 5.6106
-    assert (np.round(dragF(v_as, area=0.5089), PRECISION) == [4.8255, 12.3534, 15.6347]).all()
-    assert (np.round(dragF(v_as, rhos, area=0.63), PRECISION) == [6.9458, 25.4016, 48.2234]).all()
+    assert np.isclose(dragF(v_as[0], rhos[0], area=0.5089), 5.6106)
+    assert np.allclose(dragF(v_as, area=0.5089), [4.8255, 12.3534, 15.6347])
+    assert np.allclose(dragF(v_as, rhos, area=0.63), [6.9458, 25.4016, 48.2234])
+
 
     dx=[1, 10, 100, 0, 0]
     dy=[10, 100, 1000, 0, 1]
     gd=gradePrcnt(dy, dx)
-    assert (np.round(gd[:-1], PRECISION) == [1000, 1000, 1000, 0]).all() #and np.isnan(gd[-1]).all()
+    assert np.allclose(gd[:-1], [1000, 1000, 1000, 0])
+
 
     gd=[10, 11, 12] # grade
     mass=95
-    assert (np.round(roll_resistF(gd, mass), PRECISION) == [4.6351, 4.6303, 4.6250]).all()
+    assert np.allclose(roll_resistF(gd, mass), [4.6351, 4.6303, 4.6250])
 
-    assert (np.round(gravityF(gd, mass), PRECISION) == [92.7013, 101.8656, 111.0000]).all()
+
+    assert np.allclose(gravityF(gd, mass), [92.7013, 101.8656, 111.0000])
+
 
     vec=np.arange(10)
     vec_shift = movmean_shift(vec, 4)
     
     sol = [1., 1.5, 2.0, 3.0, 4.0,  5.0,  6.0,  7.0, np.nan, np.nan]
-    assert (np.round(vec_shift[:8], PRECISION) == sol[:8]).all() and np.isnan(vec_shift[8:]).all()
+    assert np.allclose(vec_shift[:8], sol[:8])
+    assert np.isnan(vec_shift[8:]).all()
 
 
     print('all tests passed')
--- src/utils/matfcns.py
+++ src/utils/matfcns.py
@@ -130,7 +130,7 @@ def gravityF(grade, mass):
     Computes gravity force in N (kg m/s^2)
     '''
     g = 9.8067 # gravity constant in m/s^2
-    return mass * g * np.sin(np.arctan(grade/100))
+    return mass * g * np.sin(np.arctan(np.divide(grade, 100)))
 
 def roll_resistF(grade, mass, Crr=0.005):
     '''
@@ -138,4 +138,4 @@ def roll_resistF(grade, mass, Crr=0.005):
     Crr: dimensionless coef of rolling resistance 
     '''
     g = 9.8067 # gravity constant in m/s^2
-    return Crr * mass * g * np.cos(np.arctan(grade/100))
+    return Crr * mass * g * np.cos(np.arctan(np.divide(grade, 100)))
