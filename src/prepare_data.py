7a1a6e0078dcbcb424fdbb30c4aedd9ad9d972f2
Sunuk Park <sunukp0@gmail.com>
2023-10-20 15:39:17 -0700

    for auto imputation

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 import time
@@ -7,78 +6,133 @@ from datetime import datetime, timedelta
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import plotly.graph_objects as go
-
-from statsmodels.stats.outliers_influence import variance_inflation_factor
-
-from sklearn.linear_model import LinearRegression, LassoCV
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 sys.path.append(os.path.abspath('..'))
 import src.utils.garmin_utils as garmin_utils
 from src.utils.matfcns import *
+from src.utils.garmin_activity_impute import *
 
-win_size = int(sys.argv[1])
-input_dir = '../data/garmin/raw'
-input_files = [
-    '11775496032_ACTIVITY.csv', 
-    'garmin-connect-3452356474__1692836018.csv', 
-    'garmin-connect-3452356474__1692985475.csv', 
-    'garmin-connect-3452356474__1693067992.csv', 
-    '12030209819_ACTIVITY.csv'
-    ]
-# input_files = ['12030209819_ACTIVITY.csv']
-output_dir = '../data/garmin/processed'
-output_file = 'raw'
 
-for k in range(len(input_files)):
-    output_file = f'{output_dir}/eda__raw_all_winsize_{win_size}.csv'
-    # output_file = f'{output_dir}/eda__{input_files[k]}.csv' # XXX
-    print(f'{input_files[k]}...')
+def read_config_file(config_file):
+    vars = {}
+    is_file_list = False
+    file_list = []
 
-    df = pd.read_csv(input_dir + '/' + input_files[k])
-    df = df.replace('None', np.nan) # heart_rate has this
+    with open(config_file, 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                continue
+            
+            if not is_file_list:
+                name, val = line.partition('=')[::2]
+                vars[name.strip()] = val.strip()
+                if 'input_files' == name.strip():
+                    is_file_list = True
+            else:
+                file_list.append(line.strip())
+        
+        vars['input_files'] = file_list
 
-    df['heart_rate'] = pd.to_numeric(df['heart_rate'])
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df.set_index(df['timestamp'], inplace=True)
-    df = df.drop_duplicates(subset=['timestamp'], ignore_index=True)
-    df = df.drop(['fractional_cadence'], axis=1)
-    df = df.dropna(subset=['power'])
-  
+    return vars
 
-    ##################################################################
-    # 5 movmean
-    df['power'] = movmean(df['power'], win_size)
-    df['enhanced_altitude'] = movmean(df['enhanced_altitude'], win_size)
-    df['enhanced_speed'] = movmean(df['enhanced_speed'], win_size)
-    df['cadence'] = movmean(df['cadence'], win_size)
-    df['distance'] = movmean(df['distance'], win_size)
 
-    # XXX new:
-    df['heart_rate'] = movmean(df['heart_rate'], win_size) 
-    df['temperature'] = movmean(df['temperature'], win_size)
+if __name__ == '__main__':
+    config = read_config_file(sys.argv[1])
+    input_dir = config['input_dir']
+    input_files = config['input_files']
+    output_dir = config['output_dir']
+    output_file = config['output_file']
+    win_size = int(config['win_size'])
+    tol_secs = int(config['time_tolerance'])
 
-    ##################################################################
-    # 6 derivatives
-    for col in ['timestamp', 'enhanced_altitude', 'enhanced_speed', 'cadence', 'distance']:
-        dcol = 'd_' + col
-        df[dcol] = df[col].diff()
+    
+    output_file = f'{output_dir}/{output_file}' # XXX
 
-    df = df[df['d_timestamp'] == timedelta(seconds=1)]
+    for k in range(len(input_files)):
+        print(f'{input_files[k]}...')
 
-    ##################################################################
-    # 7 grade
-    df['grade'] = gradePrcnt(df['d_enhanced_altitude'].values, df['d_distance'].values)
-    print(f'Printing to...{output_file}')
-    if k == 0:
-        df.to_csv(output_file, index=False)
-    else:
-        df.to_csv(output_file, mode='a', index=False, header=False)
+        df = pd.read_csv(input_dir + '/' + input_files[k])
+        df = df.replace('None', np.nan) # heart_rate has this
 
+        df['heart_rate'] = pd.to_numeric(df['heart_rate'])
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+        df = df.drop_duplicates(subset=['timestamp'], ignore_index=True)
+        df = df.drop(['fractional_cadence'], axis=1)
+        df = df.dropna(subset=['power'])
+
+        ##################################################################
+        # new: impute
+        df = df.reset_index(drop=True)
+        
+        # cadence
+        df.loc[np.logical_and(df['power'] == 0, df['cadence'].isna()), 'cadence'] = 0
+        first_na_locs, last_na_locs = get_first_last_na_idx(df['cadence'])
+        if len(first_na_locs)>0:
+            df['cadence'] = interpolate_vals(df, 'cadence', first_na_locs, last_na_locs, tol_secs=tol_secs)
+    
+        # position_lat/long
+        first_na_locs, last_na_locs = get_first_last_na_idx(df['position_lat'])
+        df['position_lat'] = interpolate_vals(df, 'position_lat', first_na_locs, last_na_locs, tol_secs=tol_secs)
+        df['position_long'] = interpolate_vals(df, 'position_long', first_na_locs, last_na_locs, tol_secs=tol_secs)
+
+        # distance
+        first_na_locs, last_na_locs = get_first_last_na_idx(df['distance'])
+        df['distance'] = interpolate_vals(df, 'distance', first_na_locs, last_na_locs, tol_secs=tol_secs)
+        del_locs = np.array(last_na_locs)+1
+        df = df.drop(del_locs) # delete row after null distances as that row will see distance jumped
+        df = df.reset_index(drop=True)
+
+        # enhanced_speed
+        mask = np.logical_and(np.isnan(df['enhanced_speed']), df['cadence'] == 0)
+        df.loc[mask, 'enhanced_speed'] = 0
+        first_na_locs, last_na_locs = get_first_last_na_idx(df['enhanced_speed'])
+        df['enhanced_speed'] = interpolate_vals(df, 'enhanced_speed', first_na_locs, last_na_locs, tol_secs=tol_secs)
+
+        # enhanced_altitude
+        first_na_locs, last_na_locs = get_first_last_na_idx(df['enhanced_altitude'])
+        df['enhanced_altitude'] = interpolate_vals(df, 'enhanced_altitude', first_na_locs, last_na_locs, tol_secs=tol_secs)
+
+        # heart_rate
+        first_na_locs, last_na_locs = get_first_last_na_idx(df['heart_rate'])
+        df['heart_rate'] = interpolate_vals(df, 'heart_rate', first_na_locs, last_na_locs, tol_secs=tol_secs)
+
+        # temperature
+        first_na_locs, last_na_locs = get_first_last_na_idx(df['temperature'])
+        df['temperature'] = interpolate_vals(df, 'temperature', first_na_locs, last_na_locs, tol_secs=tol_secs)
+
+        # drop all remaining na:
+        df = df.dropna()
+        df = df.reset_index(drop=True)
+
+
+        ##################################################################
+        # 5 movmean
+        df['power'] = movmean(df['power'], win_size)
+        df['enhanced_altitude'] = movmean(df['enhanced_altitude'], win_size)
+        df['enhanced_speed'] = movmean(df['enhanced_speed'], win_size)
+        df['cadence'] = movmean(df['cadence'], win_size)
+        df['distance'] = movmean(df['distance'], win_size)
+
+        # XXX new:
+        df['heart_rate'] = movmean(df['heart_rate'], win_size) 
+        df['temperature'] = movmean(df['temperature'], win_size)
+
+        ##################################################################
+        # 6 derivatives
+        for col in ['timestamp', 'enhanced_altitude', 'enhanced_speed', 'cadence', 'distance']:
+            dcol = 'd_' + col
+            df[dcol] = df[col].diff()
+
+        df = df[df['d_timestamp'] == timedelta(seconds=1)]
+        df = df.drop(['d_timestamp'], axis=1)
+
+        ##################################################################
+        # 7 grade
+        df['grade'] = gradePrcnt(df['d_enhanced_altitude'].values, df['d_distance'].values)
+
+        if k == 0:
+            df.to_csv(output_file, index=False)
+        else:
+            df.to_csv(output_file, mode='a', index=False, header=False)
+
+    print(f'Printed to...{output_file}')
---
+++ src/utils/garmin_activity_impute.py
@@ -0,0 +1,57 @@
+from datetime import datetime, timedelta
+import numpy as np
+import pandas as pd
+
+def get_first_last_na_idx(sers):
+    '''
+    Function to return the first and last indices of nan values in the Series sers.
+    The returned indices are designed to be used to interpolate missing values in between.
+    As such, the # of first indices and last indices should always match.
+    Also sers starting with a nan doesn't get counted, nor does one ending with a nan
+    (again, because we want to interpolate)
+    '''
+    isna_loc = np.argwhere(sers.isna()).ravel()
+    sers_val = sers.values
+
+    first_na_locs = []
+    last_na_locs = []
+
+    for k in isna_loc:
+        if k >= 1:
+            if not np.isnan(sers_val[k-1]):
+                first_na_locs.append(k)
+            if k < sers.shape[0]-1:
+                if not np.isnan(sers_val[k+1]):
+                    last_na_locs.append(k)
+
+
+    if np.isnan(sers_val[-1]):
+        first_na_locs = first_na_locs[:-1]
+
+    return first_na_locs, last_na_locs
+
+
+def interpolate_vals(df, col, first_na_locs, last_na_locs, tol_secs=10):
+    '''
+    Call as df[col] = interpolate_vals(df, col, ...)
+    '''
+    sers_val = df[col].values
+
+    for k in range(len(first_na_locs)):
+        
+        tdelta = df.loc[last_na_locs[k], 'timestamp'] - df.loc[first_na_locs[k], 'timestamp']
+
+        if tdelta <= timedelta(seconds=tol_secs-1):
+            nobs = last_na_locs[k] - first_na_locs[k] + 1
+            val_prev = sers_val[first_na_locs[k]-1]
+            val_next = sers_val[last_na_locs[k]+1]
+
+            rang = np.linspace(val_prev, val_next, nobs)
+
+            col_idx = df.columns.get_loc(col)
+            jj = 0
+            for j in range(first_na_locs[k], last_na_locs[k]+1):
+                # sers[j] = rang[jj]
+                df.iloc[j, col_idx] = rang[jj]
+                jj += 1
+    return df[col]
